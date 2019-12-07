5ce617da17cc9dde3cf2b0e7b46ab5a3afb73eb7
SmartRabbit42 <leleuluiz2003@gmail.com>
2019-12-07 20:16:26 -0300

    Create entry.py

---
+++ dev/client/main.py
@@ -0,0 +1,13 @@
+
+import tkinter as tk
+
+import ui.client as c
+import ui.entry as e
+
+def main():
+    window = tk.Tk()
+    app = e.Entry(window)
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
---
+++ dev/client/ui/entry.py
@@ -0,0 +1,8 @@
+""" Canary entry """
+
+import tkinter as tk
+
+class Entry(tk.Frame):
+    
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
