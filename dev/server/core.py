b192875efb51ac269b2c7225c2287dfbcb7efa68
smrtrbbt <smrtrbbt42@gmail.com>
2019-12-12 22:08:34 -0300

    Fix bugs

--- dev/server/core.py
+++ dev/server/core.py
@@ -1,10 +1,12 @@
 """ Canary server """
 
 import net.server as s
+import ui.input_handler as i
 
 def main():
     server = s.Server()
-    server.listen()
+    server.listen_async()
+    i.listen(server)
 
-if __nane__ == "__main__":
+if __name__ == "__main__":
     main()
