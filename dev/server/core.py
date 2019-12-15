2d0200621c9eefdacfd461598577e0e0e6a942a5
smrtrbbt <smrtrbbt42@gmail.com>
2019-12-15 01:28:21 -0300

    Optimize server\core

--- dev/server/core.py
+++ dev/server/core.py
@@ -1,12 +1,12 @@
 """ Canary server """
 
-import net.server as s
-import ui.input_handler as i
+from net import server
+from ui import input_handler
 
 def main():
-    server = s.Server()
-    server.listen_async()
-    i.listen(server)
+    s = server.Server()
+    s.listen_async()
+    input_handler.listen(s)
 
 if __name__ == "__main__":
     main()
