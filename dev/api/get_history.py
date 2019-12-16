8a68a7087a8751710e6cbe6ef461eb7795c8a227
smrtrbbt <smrtrbbt42@gmail.com>
2019-12-16 04:21:06 -0300

    Create get_history.py

---
+++ dev/api/get_history.py
@@ -0,0 +1,22 @@
+""" """
+
+def spotify(spotify_username):
+    history = ['spotify']
+
+    # get
+
+    return history
+
+def deezer(deezer_username):
+    history = ['deezer']
+
+    # get
+
+    return history
+
+def googleplay(googleplay_username):
+    history = ['google']
+
+    # get
+
+    return history
