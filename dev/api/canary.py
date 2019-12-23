2f720ca684e6c2826f48941e94da839401170f58
smrtrbbt <smrtrbbt42@gmail.com>
2019-12-23 02:10:06 -0300

    Update develop plan

--- dev/api/canary.py
+++ dev/api/canary.py
@@ -5,16 +5,16 @@ def suggest(history):
     # possibles is an array of
     # ["music_id":<str>, "relevance":<float>]
 
-    # for each music in history get from database music entry as A
-    # music entry loock like:
-    # [music:["name":<str>, "band":<str>], "relations":[]]
-    # where "relations" is a 1000 length list of:
-    # ["music_id":<str>, "relevance":<float>]
-    # from entry get all relations and throw in  possibles \
-    # multiplying its relevance by A relevance, if a entry of \
-    # this music already exist in possibles add its relevance multiplyed \
-    # by A music relevance to possibles entry
+    #-for each music in history get from database music entry as [A]
+    #   music entry looks like:
+    #   [music:["name":<str>, "band":<str>], "relations":[]]
+    #   where "relations" is a 1000 length list of:
+    #   ["music_id":<str>, "relevance":<float>]
+    #-from entry get all "relations" and throw in  [possibles] multiplying
+    #   its relevance by [A]'s relevance, if a entry of this music already
+    #   exists in [possibles] add its multiplyed relevance to existing
+    #   [possibles] entry
 
-    # return 100 most relevant possibles entrys
+    # return an array containing the 100 most relevant [possibles] entrys
 
     return "cu"
